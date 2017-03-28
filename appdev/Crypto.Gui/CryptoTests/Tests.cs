09a6f9ded20dc48f9c9fcf625eaec970667c9c84
RobinGoos <goosrobin1@gmail.com>
2017-03-28 14:28:48 +0200

    Added AES+DES classes and tests

---
+++ appdev/Crypto.Gui/CryptoTests/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Crypto.encrypt;
+
+namespace CryptoTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ShouldDecryptMessageDES()
+        {
+            String message = "HelloWorld";
+            String encrypted = DES.Encrypt(message);
+            Assert.That(DES.Decrypt(encrypted).Equals("HelloWorld"));
+        }
+        [Test]
+        public void ShouldDecryptMessageAES()
+        {
+            String message = "HelloWorld";
+            String encrypted = AES.Encrypt(message, "Passwoordzin", "salt", "SHA1", 2, "abcdefghijlmnopq", 256);
+            Assert.That(AES.Decrypt(encrypted, "Passwoordzin", "salt", "SHA1", 2, "abcdefghijlmnopq", 256).Equals(message));
+        }
+    }
+}
+
